cdf640c9808b21352bdd9eb03390f341ae221950
pocketmun1216 <pocketmun1216@gmail.com>
2024-07-06 13:38:36 +0900

    input Servlet 작성

--- src/main/java/sec01/ex01/FirstServlet.java
+++ src/main/java/sec01/ex01/FirstServlet.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
 
 	@Override
