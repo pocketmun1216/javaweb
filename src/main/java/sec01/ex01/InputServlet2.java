aa03353d1b1c19ad55ce71ccbc0e3f1bed68f81c
pocketmun1216 <pocketmun1216@gmail.com>
2024-07-06 13:43:33 +0900

    enum 예제 추가
    enum 예제 추가

---
+++ src/main/java/sec01/ex01/InputServlet2.java
@@ -0,0 +1,46 @@
+package sec01.ex01;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * Servlet implementation class InputServlet
+ */
+@WebServlet("/input2")
+public class InputServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InputServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		System.out.println("아이디 : " + user_id);
+		System.out.println("패스워드 : " + user_pw);
+		Enumeration enu = request.getParameterNames();
+		
+		while (enu.hasMoreElements()) {
+			String name = (String) enu.nextElement();
+			String[] values = request.getParameterValues(name);
+			
+			for (String value : values) {
+				System.out.println("name = " + name + ", value = " + value);
+			}
+		}
+	}
+}
